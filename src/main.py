b514895bce419da3f703d880fccc5e3ad2cc35e2
Pavel <shegol164@mail.ru>
2025-03-11 12:32:00 +0400

    add main.py

---
+++ src/main.py
@@ -0,0 +1,22 @@
+from src.utils import load_data_from_json
+from src.category import Category
+
+def main():
+    # Загрузка данных из JSON
+    categories = load_data_from_json(r"C:\Users\Pavel\PycharmProjects\oop_homework\data\products.json")
+
+    # Вывод информации о категориях и товарах
+    for category in categories:
+        print(f"Категория: {category.name}")
+        print(f"Описание: {category.description}")
+        print(f"Количество товаров: {len(category.products)}")
+        for product in category.products:
+            print(f"  Товар: {product.name}, Цена: {product.price}, Количество: {product.quantity}")
+        print()
+
+    # Вывод общей статистики
+    print(f"Всего категорий: {Category.category_count}")
+    print(f"Всего товаров: {Category.product_count}")
+
+if __name__ == "__main__":
+    main()
