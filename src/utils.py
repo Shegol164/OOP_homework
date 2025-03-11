90b9fdc0158b4be74f6d4612c0eac44787d33da1
Pavel <shegol164@mail.ru>
2025-03-11 12:32:15 +0400

    add utils.py

---
+++ src/utils.py
@@ -0,0 +1,39 @@
+import json
+from typing import List
+from src.product import Product
+from src.category import Category
+
+
+def load_data_from_json(filepath: str) -> List[Category]:
+    """
+    Загружает данные о категориях и товарах из JSON-файла.
+    :param filepath: Путь к JSON-файлу.
+    :return: Список объектов Category.
+    """
+    with open(filepath, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    categories = []
+    for category_data in data:
+        products = [
+            Product(
+                name=product_data["name"],
+                description=product_data["description"],
+                price=product_data["price"],
+                quantity=product_data["quantity"],
+            )
+            for product_data in category_data["products"]
+        ]
+        category = Category(
+            name=category_data["name"],
+            description=category_data["description"],
+            products=products,
+        )
+        categories.append(category)
+
+    return categories
+
+if __name__ == "__main__":
+    categories = load_data_from_json(r"C:\Users\Pavel\PycharmProjects\oop_homework\data\products.json")
+    for category in categories:
+        print(f"Категория: {category.name}, Товаров: {len(category.products)}")
