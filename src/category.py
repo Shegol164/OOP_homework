6dd6dda08d1123e789d5d170e04d7e3079e13622
Pavel <shegol164@mail.ru>
2025-03-11 12:31:50 +0400

    add category.py

---
+++ src/category.py
@@ -0,0 +1,20 @@
+class Category:
+    """Класс для представления категории товаров."""
+
+    # Атрибуты класса
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name: str, description: str, products: list, ):
+        """
+        Инициализация категории.
+        :param name: Название категории.
+        :param description: Описание категории.
+        :param products: Список товаров в категории.
+        """
+        self.name = name
+        self.description = description
+        self.products = products
+        # Обновление атрибутов класса
+        Category.category_count += 1
+        Category.product_count += len(products)
