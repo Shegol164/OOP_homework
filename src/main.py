564630ee85d3f29012170888eed62018fea0674e
Pavel <shegol164@mail.ru>
2025-03-21 14:22:21 +0400

    add main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from src.category import Category
 from src.utils import load_data_from_json
+from src.product import Product
 
 
 def main() -> None:
@@ -16,10 +17,30 @@ def main() -> None:
         print("Товары:")
         print(category.products)
         print()
+    print("#" * 119)
 
     # Вывод общей статистики
     print(f"Всего категорий: {Category.category_count}")
     print(f"Всего товаров: {Category.product_count}")
+    print("#" * 119)
+
+    # Вывод информации о категориях и товарах
+    for category in categories:
+        print(category)  # Используем __str__ для категории
+        print("Товары:")
+        for product in category._Category__products:  # Итерация по товарам
+            print(product)  # Используем __str__ для продукта
+        print()
+
+    # Пример сложения продуктов
+    product1 = Product("Смартфон",
+                       "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
+                       180000.0, 5)
+    product2 = Product("Телевизоры",
+                       "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+                       123000.0, 7)
+    total_value = product1 + product2
+    print(f"Общая стоимость товаров: {total_value} руб.")
 
 
 if __name__ == "__main__":
