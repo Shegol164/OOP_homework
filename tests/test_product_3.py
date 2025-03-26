1ecf07951e280ab0f89fa6f4bfbbb54995379b18
Pavel <shegol164@mail.ru>
2025-03-26 14:51:03 +0400

    add test_product_3.py

--- tests/test_product_3.py
+++ tests/test_product_3.py
@@ -12,7 +12,13 @@ def test_product_creation(capsys):
     assert str(p) == "Телефон, 10000 руб. Остаток: 5 шт."
 
 def test_smartphone_inheritance():
-    s = Smartphone("iPhone", "Флагман", 80000, 1,
-                  "A15", "13", "128GB", "Black")
+    s = Smartphone("Samsung Galaxy S23 Ultra",
+        "256GB, Серый цвет, 200MP камера",
+        180000.0,
+        5,
+        95.5,
+        "S23 Ultra",
+        256,
+        "Серый")
     assert isinstance(s, Product)
     assert isinstance(s, BaseProduct)
